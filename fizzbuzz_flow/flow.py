75805935106719605420b736c1c2ccc4a973fd53
Thomas K <thomas.klar@tuwien.ac.at>
2025-05-26 12:24:34 +0200

    Auto-push before running flow 'fizzbuzz_flow' at 2025-05-26T12:24:34.233053

--- fizzbuzz_flow/flow.py
+++ fizzbuzz_flow/flow.py
@@ -1,19 +1,27 @@
 from prefect import flow, task
 from .task1 import main as imported_task1
+import pandas as pd
 
 @task
-def task1():
-    return imported_task1()
+def task1(arg1):
+    df = pd.read_csv('data_flow1/steam_games_dataset.csv', nrows=5)
+    print(df)
+    return imported_task1(arg1)
 
 
 @flow
-def run_flow():
+def run_flow(arg1, arg2):
     version = "1.0.1"
     print("Hello World!")
     task1()
     print("Bye World!")
     print("This is version {version}")
+    print(f"Received {arg1, arg2}")
     return
 
+def run(arg1, arg2):
+    run_flow(arg1, arg2)
+
+
 if __name__ == "__main__":
-    run_flow()
+    run()
