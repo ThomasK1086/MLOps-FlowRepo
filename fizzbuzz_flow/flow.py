bb1d08c2a66bec1e0699e0dfa1193dbb8c4de672
Thomas K <thomas.klar@tuwien.ac.at>
2025-05-26 12:48:53 +0200

    Auto-push before running flow '{flow_name}' at {datetime.now().isoformat()}

--- fizzbuzz_flow/flow.py
+++ fizzbuzz_flow/flow.py
@@ -16,7 +16,7 @@ def task1(arg1):
 def run_flow(arg1, arg2):
     version = "1.0.1"
     task1(arg1)
-    print("This is version {version}")
+    print(f"This is version {version}")
     print(f"Received {arg1, arg2}")
     return
 
