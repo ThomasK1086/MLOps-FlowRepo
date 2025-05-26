df80fabd3ea1ae455f49dbfe8652dfef4d609bbb
Thomas K <thomas.klar@tuwien.ac.at>
2025-05-26 11:24:31 +0200

    Automated commit to subfolder_1

--- fizzbuzz_flow/flow.py
+++ fizzbuzz_flow/flow.py
@@ -8,9 +8,11 @@ def task1():
 
 @flow
 def run_flow():
+    version = "1.0.1"
     print("Hello World!")
     task1()
     print("Bye World!")
+    print("This is version {version}")
     return
 
 if __name__ == "__main__":
