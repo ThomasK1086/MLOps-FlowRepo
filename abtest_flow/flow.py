a032f6c7e7055a277e1770bc70b4052e0fa39810
Thomas K <thomas.klar@tuwien.ac.at>
2025-05-27 12:29:49 +0200

    Auto-push before running flow 'abtest_flow' at 2025-05-27T12:29:49.547908

---
+++ abtest_flow/flow.py
@@ -0,0 +1,178 @@
+from prefect import flow, task
+from datetime import datetime
+timestamp = datetime.now()
+from pathlib import Path
+import warnings
+warnings.filterwarnings("ignore")
+from prefect.artifacts import create_markdown_artifact, get_run_context
+from prefect.client.schemas.filters import FlowRunFilter, FlowRunFilterId
+from prefect.client.orchestration import get_client
+
+import asyncio
+import os
+import json
+import sys
+import re
+import cloudpickle
+import base64
+import hashlib
+
+
+
+
+from task1 import main as run_ab_test
+
+
+@task(
+    name="Step 1 of AB Test Flow"
+)
+def step_one(*args, **kwargs):
+    return run_ab_test(*args, **kwargs)
+
+def get_artifact(flow_run_id: str) -> dict:
+    async def _read_json_from_artifact():
+        result = None
+        async with get_client() as client:
+            artifacts = await client.read_artifacts(
+                flow_run_filter=FlowRunFilter(
+                    id=FlowRunFilterId(any_=[flow_run_id])
+                )
+            )
+            if not artifacts or len(artifacts) == 0:
+                raise ValueError(f"Encountered no artifacts for given flow run id: {flow_run_id}")
+            elif len(artifacts) > 1:
+                raise ValueError(f"Encountered more than one artifact for given flow run id: {flow_run_id}")
+
+            artifact = artifacts[0]
+
+            # Extract JSON from markdown code block
+            match = re.search(r"```json\s*\n(.*?)\n```", artifact.data, re.DOTALL)
+            if match:
+                try:
+                    result = json.loads(match.group(1))
+                except json.JSONDecodeError as e:
+                    print(f"[ERROR] JSON decode error: {e}")
+            else:
+                print("[ERROR] No JSON code block found")
+        return result
+
+    artifact = asyncio.run(_read_json_from_artifact())
+
+    if not artifact:
+        raise FileNotFoundError(f"Could not load artifact for flow run ID '{flow_run_id}'")
+
+    return artifact
+
+
+@flow(
+    name="MLOpsEx3 AB-Test Flow",
+    flow_run_name=f"MLOpsEx3 model A/B test flow at {timestamp.strftime('%Y%m%d-%H%M')}",
+    description="This flow performs an A/B test one two different models, splitting input data randomly.",
+    version="1.0.0",
+    retries=0,
+    timeout_seconds=600
+)
+def myflow_runner(
+        working_dir,
+        dataset_name,
+        flow_run_id_A,
+        flow_run_id_B,
+        hash_function_string = None,
+        split_function_string = None,
+        seed = 42,
+        cutoff_year=2020,
+        commit_id=None
+):
+    A_artifact = get_artifact(flow_run_id_A)
+    A_modelpath = A_artifact["model_path_full"]
+
+    B_artifact = get_artifact(flow_run_id_B)
+    B_modelpath = B_artifact["model_path_full"]
+
+    ab_testresults = step_one(
+        Path(working_dir),
+        dataset_name,
+        A_modelpath,
+        B_modelpath,
+        hash_function_string,
+        split_function_string,
+        seed,
+        cutoff_year,
+    )
+
+
+    flow_id = get_run_context().flow_run.id
+
+    metadata = {
+        "flow_run_id": str(flow_id),
+        "kwargs": {
+            "working_dir": working_dir,
+            "dataset_name": dataset_name,
+            "flow_run_id_A": flow_run_id_A,
+            "flow_run_id_B": flow_run_id_B,
+            "hash_function_string": hash_function_string,
+            "split_function_string": split_function_string,
+            "seed": seed,
+            "cutoff_year": cutoff_year,
+        },
+        "Results": ab_testresults,
+        "git_commit_hexsha": commit_id,
+        "timestamp_start": timestamp.isoformat(),
+        "timestamp_end": datetime.now().isoformat(),
+    }
+
+    # Convert to JSON string
+    metadata_json = json.dumps(metadata, indent=2)
+
+    # Create artifact with JSON content
+    artifact_id = create_markdown_artifact(
+        key="abtest-flow",
+        markdown=f"```json\n{metadata_json}\n```",
+        description="Flow metadata serialized as JSON"
+    )
+
+    # Dump the artifact locally too
+    with open("Flow_Artifacts_Local.txt", "a+", encoding="utf-8") as f:
+        f.write(metadata_json)
+
+    with open("Flow_Ids.txt", "a+", encoding="utf-8") as f:
+        f.write(str(flow_id))
+
+
+    return flow_id, artifact_id
+
+
+
+def main():
+    if len(sys.argv) > 1:
+        try:
+            # sys.argv[1] is a JSON string
+            param_blob = sys.argv[1]
+            params = json.loads(param_blob)
+            args = params.get("args", [])
+            kwargs = params.get("kwargs", {})
+            commit_id = params.get("commit_id", None)
+        except Exception as e:
+            print(f"❌ Failed to parse args: {e}")
+            args = []
+            kwargs = {}
+            commit_id = None
+    else:
+        args = []
+        kwargs = {}
+        commit_id = None
+
+
+
+    #print(f"🚀 Running with args={args} kwargs={kwargs}")
+    #prefect_url = os.getenv("PREFECT_API_URL")
+    #mlflow_uri = os.getenv("MLFLOW_TRACKING_URI")
+    #print(f"Env variables are {prefect_url} and {mlflow_uri}")
+    kwargs.update({"commit_id": commit_id})
+    flow_id, artifact_id = myflow_runner(*args, **kwargs)
+
+    print(f"Flow ID: {flow_id}, Artifact ID: {artifact_id}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ abtest_flow/task1.py
@@ -0,0 +1,116 @@
+import mlflow.sklearn
+import pandas as pd
+from pathlib import Path
+from sklearn.metrics import balanced_accuracy_score, accuracy_score, f1_score
+
+from evidently.test_suite import TestSuite
+from evidently.tests import TestAccuracyScore, TestF1Score, TestRecallByClass
+from evidently.test_preset import MulticlassClassificationTestPreset, DataDriftTestPreset, DataStabilityTestPreset
+
+import hashlib
+import base64
+import cloudpickle
+
+def main(
+        infile_dir,
+        infile_name,
+        A_modelpath,
+        B_modelpath,
+        hash_function_string=None,
+        split_function_string=None,
+        seed=42,
+        cutoff_year=2020,
+):
+    if hash_function_string is not None:
+        try:
+            func_bytes = base64.b64decode(hash_function_string.encode("utf-8"))
+            hash_fn = cloudpickle.loads(func_bytes)
+        except:
+            raise ValueError("Could not load hash function from pickle file. Did you pass a cloudpickle.dumps object?")
+        else:
+            def hash_fn(datetime, seed):
+                """Generates a hash from datetime and a seed, then returns an float between 0 and 1."""
+                data_str = f"{seed}_{datetime}"
+                hash_obj = hashlib.sha256(data_str.encode('utf-8'))
+                hash_int = int(hash_obj.hexdigest(), 16)
+                return hash_int % 1000 / 1000.0
+
+    if split_function_string is not None:
+        try:
+            func_bytes = base64.b64decode(split_function_string.encode("utf-8"))
+            split_fn = cloudpickle.loads(func_bytes)
+        except:
+            raise ValueError("Could not load splitter function from pickle file. Did you pass a cloudpickle.dumps object?")
+    else:
+        def split_fn(h, seed):
+            if h < 0.33:
+                return -1
+            elif h < 0.66:
+                return 0
+            else:
+                return 1
+
+
+    input_path = infile_dir / infile_name
+    dataset = pd.read_csv(input_path, index_col=False)
+    dataset.release_date = pd.to_datetime(dataset.release_date)
+
+    input_cols = ['release_date', 'price', 'positive_reviews', 'negative_reviews', 'metacritic_score', 'peak_ccu', 'recommendations', 'required_age', 'on_linux', 'on_mac', 'on_windows']
+    X, y = dataset[input_cols], dataset['estimated_owners']
+
+    mask = dataset.release_date.dt.year >= cutoff_year
+
+    X_new = X[mask]
+    y_new = y[mask]
+
+    X_new['hash'] = X_new.release_date.apply(hash_fn, seed)
+    X_new['group'] = X_new.hash.apply(split_fn, seed)
+
+    mask_A = X_new['group'] == -1
+    mask_B = X_new['group'] == 1
+
+    X_A = X_new[mask_A]
+    y_A = y_new[mask_A]
+
+    X_B = X_new[mask_B]
+    y_B = y_new[mask_B]
+
+
+    X_A.drop(columns=['group', 'hash', 'release_date'], inplace=True)
+    X_B.drop(columns=['group', 'hash', 'release_date'], inplace=True)
+
+    model_A = mlflow.sklearn.load_model(A_modelpath)
+    model_B = mlflow.sklearn.load_model(B_modelpath)
+
+    y_pred_A = model_A.predict(X_A)
+    y_pred_B = model_B.predict(X_B)
+
+    accuracy_A = accuracy_score(y_A, y_pred_A)
+    f1_A = f1_score(y_A, y_pred_A, average='macro')
+    balanced_accuracy_A = balanced_accuracy_score(y_A, y_pred_A)
+
+    accuracy_B = accuracy_score(y_B, y_pred_B)
+    f1_B = f1_score(y_B, y_pred_B, average='macro')
+    balanced_accuracy_B = balanced_accuracy_score(y_B, y_pred_B)
+
+    return {
+        "results_A" : {
+            "accuracy": accuracy_A,
+            "balanced_accuracy": balanced_accuracy_A,
+            "f1": f1_A,
+            "groupsize": len(mask_A),
+            "model": A_modelpath
+        },
+        "results_B" : {
+            "accuracy": accuracy_B,
+            "balanced_accuracy": balanced_accuracy_B,
+            "f1": f1_B,
+            "groupsize": len(mask_B),
+            "model": B_modelpath
+        }
+    }
+
+if __name__ == "__main__":
+    pass
+
+
